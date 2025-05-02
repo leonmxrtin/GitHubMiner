ec9a2ff0d24a418ee243cc4f306410b4667b0691
León Martín <leonmartinl@icloud.com>
2025-05-02 13:16:11 +0200

    initial

---
+++ src/main/java/aiss/githubminer/GitHubMinerApplication.java
@@ -0,0 +1,13 @@
+package aiss.githubminer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubMinerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubMinerApplication.class, args);
+    }
+
+}
