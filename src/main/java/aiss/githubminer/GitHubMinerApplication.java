156180cc024c6bfbeea37d8a4c1cf2a2cd9dd554
León Martín <leonmartinl@icloud.com>
2025-05-11 00:49:58 +0200

    cache user data to avoid unnecesary requests

--- src/main/java/aiss/githubminer/GitHubMinerApplication.java
+++ src/main/java/aiss/githubminer/GitHubMinerApplication.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableCaching
 public class GitHubMinerApplication {
 
     @Value("${github.token}")
--- src/main/java/aiss/githubminer/service/UserService.java
+++ src/main/java/aiss/githubminer/service/UserService.java
@@ -2,6 +2,7 @@ package aiss.githubminer.service;
 
 import aiss.githubminer.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,7 +12,9 @@ public class UserService {
     @Autowired
     RestTemplate restGitHub;
 
+    @Cacheable("users")
     public User getUser(String username) {
+        System.out.println("Getting user " + username);
         return restGitHub.getForObject("/users/" + username, User.class);
     }
 }
