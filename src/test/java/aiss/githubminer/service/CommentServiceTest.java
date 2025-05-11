baa01c8c0136baf20c54ff0caf9741615fb267ef
León Martín <leonmartinl@icloud.com>
2025-05-11 18:57:19 +0200

    complete CommentServiceTest

--- src/test/java/aiss/githubminer/service/CommentServiceTest.java
+++ src/test/java/aiss/githubminer/service/CommentServiceTest.java
@@ -1,7 +1,6 @@
 package aiss.githubminer.service;
 
 import aiss.githubminer.model.Comment;
-import aiss.githubminer.model.Issue;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,11 +16,18 @@ class CommentServiceTest {
     void getIssueComments() {
         String owner = "spring-projects";
         String repo = "spring-framework";
-        String issueId = "34878";
-        Integer sinceDays = 3;
+        String issueId = "22968";
+        Integer sinceDays = 3650;
         Integer maxPages = 2;
+
+        String expectedCommentId = "492051943";
+        String expectedCommentBody = "Spring Framework can work in [GraalVM](https://www.graalvm.org/) mode will be excited. And Which spring framework release version do we plan to support GraalVM with native image? 5.X Or ? Thanks ";
+
+
         List<Comment> comments = commentService.getIssueComments(owner, repo, issueId, sinceDays, maxPages);
         assertNotNull(comments);
+        assertFalse(comments.isEmpty());
+        assertTrue(comments.stream().anyMatch(comment -> comment.getId().equals(expectedCommentId) && comment.getBody().equals(expectedCommentBody)));
         System.out.println(comments);
     }
 }
